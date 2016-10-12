d01216d9688e55ebbb68af04b33d1c74a79ea7d6
erick <erick@DESKTOP-7DP9Q9N>
2016-10-12 13:29:29 +0700

    menambahkan services mahasiswa

---
+++ SampleASPNETClient/Services/MahasiswaServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestSharp;
+using SampleASPNETClient.Models;
+
+namespace SampleASPNETClient.Services
+{
+    public class MahasiswaServices
+    {
+        private RestClient _client;
+
+        public MahasiswaServices()
+        {
+            _client = new RestClient("http://localhost:52099/");
+        }
+    }
+}
