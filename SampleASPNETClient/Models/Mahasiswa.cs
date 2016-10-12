79df6215b04bae5c6442277bb3115a20c4b3f99c
erick <erick@DESKTOP-7DP9Q9N>
2016-10-12 16:13:47 +0700

    menambahkan validation summary

--- SampleASPNETClient/Models/Mahasiswa.cs
+++ SampleASPNETClient/Models/Mahasiswa.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using System.ComponentModel.DataAnnotations;
+
 namespace SampleASPNETClient.Models
 {
     public class Mahasiswa
     {
+        [Required]
         public string Nim { get; set; }
+
+        [Required]
         public string Nama { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
         public double IPK { get; set; }
     }
 }
