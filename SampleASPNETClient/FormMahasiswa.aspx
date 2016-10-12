4ab76a643dfc73e10eadd27220a7869e3fda0b3a
erick <erick@DESKTOP-7DP9Q9N>
2016-10-12 13:54:14 +0700

    menambahkan form mahasiswa

---
+++ SampleASPNETClient/FormMahasiswa.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="FormMahasiswa.aspx.cs" Inherits="SampleASPNETClient.FormMahasiswa" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Daftar Mahasiswa</h2>
+
+    <asp:GridView CssClass="table table-striped" runat="server" ID="gvMahasiswa"
+        ItemType="SampleASPNETClient.Models.Mahasiswa"
+        SelectMethod="gvMahasiswa_GetData"></asp:GridView>
+</asp:Content>
---
+++ SampleASPNETClient/FormMahasiswa.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using SampleASPNETClient.Models;
+using SampleASPNETClient.Services;
+
+namespace SampleASPNETClient
+{
+    public partial class FormMahasiswa : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+      
+        public IEnumerable<Mahasiswa> gvMahasiswa_GetData()
+        {
+            MahasiswaServices mhsServices = new MahasiswaServices();
+            return mhsServices.GetAll();
+        }
+    }
+}
