8b91888d11bd950431d43c6c0f974887d1e84256
erick <erick@DESKTOP-7DP9Q9N>
2016-10-14 11:24:37 +0700

    menambahkan logut

---
+++ SampleASPNETClient/Logout.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SampleASPNETClient
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Session["ProfilPengguna"]!=null)
+            {
+                Session["ProfilPengguna"] = null;
+                Session.Clear();
+                Response.Redirect("~/FormLogin");
+            }
+        }
+    }
+}
